8a36f86aec8fc3018269d811acab38cfb8421917
Ed_ValG_ViTTo <eduardprogramist@gmail.com>
2019-07-09 23:41:12 +0600

    the end 2

---
+++ 404.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>404</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <link type="text/css" media="all" href="css/404.css" rel="stylesheet" />
+    <link type="text/css" media="all" href="css/respons.css" rel="stylesheet" />
+    <link href='https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+    <div class="animationload">
+        <div class="loader">
+        </div>
+    </div>
+    <div id="wrapper">
+        <div class="container">
+            <div class="switcher">
+                <input id="sw" type="checkbox" class="switcher-value">
+                <label for="sw" class="sw_btn"></label>
+                <div class="bg"></div>
+                <div class="text">
+                	<span class="text-l">On</span>
+                	<span class="text-d">Off</span>
+                	<br/>light
+                </div>
+            </div>
+            <div id="dark" class="row text-center">
+                <div class="info">
+                    <img src="images/404.png" alt="404 error" />
+                </div>
+                    <a href="index.php" class="btn btn-info">Главная</a>
+            </div>
+            <div id="light" class="row text-center">
+                <div class="info">
+                    <img src="images/404.gif" alt="404 error" />
+                    <h4>Лучше не ищите страницу в адресной строке, страница которую вы ищете, в полне возможно была перемещена, удалена, переименована или никогда не существовала!)</h4>
+                    <a href="index.php" class="btn btn-brown">Главная</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/modernizr.custom.js" type="text/javascript"></script>
+    <script src="js/jquery.nicescroll.min.js" type="text/javascript"></script>
+    <script src="js/404.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ 500.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>500</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,700" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="css/500.css" />
+</head>
+
+<body>
+
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>Oops!</h1>
+				<h2>500 - ошибка сервера</h2>
+			</div>
+			<a href="index.php">На главную?</a>
+		</div>
+	</div>
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
